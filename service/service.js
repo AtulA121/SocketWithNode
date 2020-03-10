70d26fc7d57b47b44973cdf33078e7ed99e9d270
AtulA121 <54090342+AtulA121@users.noreply.github.com>
2020-03-10 11:02:37 +0530

    Update service.js

--- service/service.js
+++ service/service.js
@@ -5,6 +5,7 @@ let conn={
     verifyToken : async(token)=>{
         return new Promise((resolve,reject)=>{
             jwt.verify(token, constants.jwtKey, async(err, verifiedJwt) => {
+                console.log(verifiedJwt);
                 if(err){
                     reject(err);
                 }else{
