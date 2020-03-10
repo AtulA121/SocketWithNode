ca84202d0ce1f4103035fd6f823dab1efeac6c97
Hitesh Fuse <hitesh.fuse@codevian.com>
2020-03-10 11:17:43 +0530

    third time

--- service/service.js
+++ service/service.js
@@ -5,7 +5,6 @@ let conn={
     verifyToken : async(token)=>{
         return new Promise((resolve,reject)=>{
             jwt.verify(token, constants.jwtKey, async(err, verifiedJwt) => {
-                console.log(verifiedJwt);
                 if(err){
                     reject(err);
                 }else{
